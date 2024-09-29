c467799c1dc3c48e1ffc34f62594650eca134150
FazleRahmanEvan <evanshakib26@gmail.com>
2024-09-30 00:56:25 +0600

    academy

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -103,7 +103,7 @@ const studentSchema = new Schema<TStudent>(
       },
       required: [true, "Gender is required"],
     },
-    dateOfBirth: { type: String },
+    dateOfBirth: { type: Date },
     email: {
       type: String,
       required: [true, "Email is required"],
@@ -138,7 +138,10 @@ const studentSchema = new Schema<TStudent>(
       required: [true, "Local guardian information is required"],
     },
     profileImg: { type: String },
-
+    admissionSemester: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicSemester",
+    },
     isDeleted: {
       type: Boolean,
       default: false,
